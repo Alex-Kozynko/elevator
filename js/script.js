41e5974d998d6e360c4a0ffa17623ec81128718b
Alex-front-end-developer <alexandrkozynko@gmai.com>
2019-05-17 10:18:06 +0200

    last commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ $('.slider').slick({
 	variableWidth: true,
 	infinite: false
 });
+
+$('.floating').click(function () {
+	$(this).find('p span').slideToggle();
+	$(this).toggleClass('active');
+});
+
+$(window).on("load", function() {
+	var wow = new WOW({
+		offset: 60
+	})
+	wow.init();
+});
+
+if ( $(window).width() < 999 ) {
+	$('.page_wrapper, .certificate .container, .portfolio .container .wrapper').slick({
+		centerMode: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		variableWidth: true,
+		infinite: false
+	});
+};
